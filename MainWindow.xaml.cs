354517f1e7514fbdc053b70b42519f59a114b318
vorvit <94832434+vorvit@users.noreply.github.com>
2021-12-19 01:06:29 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+                textBox.FontFamily = new FontFamily(fontName);
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textBox != null)
+                textBox.FontSize = fontSize;
+        }
+
+        private void btnBold_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+                textBox.FontWeight = FontWeights.Bold;
+            else
+                textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void btnItalic_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+                textBox.FontStyle = FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void btnUnderLine_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == TextDecorations.Baseline)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+                textBox.TextDecorations = TextDecorations.Baseline;
+        }
+
+        private void rbtnRed_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Red;
+        }
+
+        private void rbtnBlack_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Black;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openDialog.ShowDialog() == true)
+                textBox.Text = File.ReadAllText(openDialog.FileName);
+        }
+        private void MenuItem_Click1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveDialog.ShowDialog() == true)
+                File.WriteAllText(saveDialog.FileName, textBox.Text);
+        }
+        private void MenuItem_Click2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
